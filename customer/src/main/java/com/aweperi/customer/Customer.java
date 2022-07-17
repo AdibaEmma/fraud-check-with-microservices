c277d83dbd4a269b18cab38e3bb8bb7dc8d31d86
Emmanuel Adiba <eabaagah@gmail.com>
2022-07-17 17:14:13 +0000

    add customer repository

--- customer/src/main/java/com/aweperi/customer/Customer.java
+++ customer/src/main/java/com/aweperi/customer/Customer.java
@@ -1,11 +1,24 @@
 package com.aweperi.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Integer id;
     private String firstName;
     private String lastName;
